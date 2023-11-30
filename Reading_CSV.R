03ce1b8b832e7848abd45dbca1b98f17cee16c4f
Odhawijesse <jesseOdhawi@gmail.com>
2023-11-29 23:09:46 -0800

    Second commit

--- Reading_CSV.R
+++ Reading_CSV.R
@@ -1,5 +1,12 @@
 # Read the CSV file "data.csv"
-data <- read.csv("data.csv")
+data <- read.csv("doaj-article-sample.csv")
+
+#print(head(data))
 
 # Print the column names
 print(colnames(data))
+print(names(data))
+
+# Print the data of each column
+#  print(data[[column]])
+#}
